9eba394380ced0f97728cd2c4dd2c51f6b7b22fa
tsukimi <128211112+hikawi@users.noreply.github.com>
2024-09-17 17:57:05 +0700

    basic theme switcher

---
+++ src/components/ThemeSwitcher.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import IconMoon from "./icons/IconMoon.vue";
+import IconSun from "./icons/IconSun.vue";
+
+function toggleTheme() {
+  document.documentElement.classList.toggle("dark");
+  console.log("Hello", document.documentElement.classList);
+}
+</script>
+
+<template>
+  <div class="flex flex-row gap-1 sm:gap-4">
+    <IconSun />
+    <button
+      aria-label="Dark mode, light mode switcher"
+      class="bg-purple relative flex w-8 flex-row items-center justify-start rounded-full p-1 sm:w-12 dark:justify-end"
+      @click="toggleTheme"
+    >
+      <span class="size-3 rounded-full bg-white sm:size-5"></span>
+    </button>
+    <IconMoon />
+  </div>
+</template>
---
+++ tailwind.config.mjs
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: "selector",
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      screens: {
+        xs: "360px",
+      },
+      colors: {
+        purple: "#A729F5",
+        "dark-navy": "#313E51",
+        navy: "#3B4D66",
+        "gray-navy": "#626C7F",
+        "light-blue": "#ABC1E1",
+        "light-gray": "#F4F6FA",
+        white: "#FFFFFF",
+        green: "#26D782",
+        red: "#EE5454",
+      },
+      fontFamily: {
+        rubik: ["Rubik", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
